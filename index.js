23b3629b19107aa77fe1f6b8f9413c3dd73cb222
Fabien Marceau <fabien.marceau@gmail.com>
2023-01-27 22:49:44 +0100

    avancée

--- index.js
+++ index.js
@@ -4,16 +4,27 @@ Vue.component("product_ordered", {
             <p class="product_order_txt">Votre choix: {{ nom }}</p>
         <div class="sucre">
             <button v-on:click ="sucrer()" class="sugar">Sucré</button>
-             <button class="sugar">Non Sucré</button>
              </div>
         </div>`,
   props: ["nom"],
     methods: {
         sucrer: function () {
-            this.$emit('balance')
+            this.$emit("go");
         }
     }  
 });
+
+// Vue.component("sugar_selection", {
+//   template: `
+//         <div class = "container_sugarSelection">
+//     <input type="range" v-on:click="range()"></input> 
+//         </div>`,
+//   methods: {
+//    range: function () {
+//       this.$emit("bam");
+//     },
+//   },
+// });
 
 var app = new Vue({
   el: "#app",
@@ -22,13 +33,14 @@ var app = new Vue({
     panier: [],
   },
 
-  methods: {
-    order: function (produits) {
-      console.log(produits);
-      this.panier.push(produits);
-      },
-      sugar_option: function () {
-          console.log("ok");
-      }
-  },
+    methods: {
+        order: function (produits) {
+            console.log(produits);
+            this.panier.push(produits);
+        },
+        sugar_option: function () {
+            console.log("ok");
+            
+        },
+    }
 });
