cde9160a8c5fc90fc0b76d13292c0d103ae7ed81
Fabien Marceau <fabien.marceau@gmail.com>
2023-01-27 21:37:02 +0100

    compréhension composant

--- index.js
+++ index.js
@@ -1,28 +1,34 @@
-Vue.component('product_ordered',{
-    template:`
+Vue.component("product_ordered", {
+  template: `
         <div class = "container_ordered">
             <p class="product_order_txt">Votre choix: {{ nom }}</p>
-            <input type = "number"></input>
-        </div>`
-    ,
-    props: ['nom'],
-    // methods: {
-    //     passer_commande: function (produits) {
-    //         this.$emit('commande-passée' , produits)
-    //     }
-})
+        <div class="sucre">
+            <button v-on:click ="sucrer()" class="sugar">Sucré</button>
+             <button class="sugar">Non Sucré</button>
+             </div>
+        </div>`,
+  props: ["nom"],
+    methods: {
+        sucrer: function () {
+            this.$emit('balance')
+        }
+    }  
+});
 
 var app = new Vue({
   el: "#app",
   data: {
-    array: ["carottes", "steack", "cake", "burger", "coffe", "granola", "sirop", "Grenadine", "Cacolac", "Pacalo"],
-    panier: [ ],
+    array: ["Espresso", "Double Espresso", "Cappucino", "Américain", "Allongé", "Ristretto", "Thé", "Infusion", "Chocolat chaud"],
+    panier: [],
   },
 
   methods: {
     order: function (produits) {
       console.log(produits);
       this.panier.push(produits);
-    },
+      },
+      sugar_option: function () {
+          console.log("ok");
+      }
   },
 });
