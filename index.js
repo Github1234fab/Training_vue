86e7a784166ad4dc60523fbfe3cf3767ba12728d
Fabien Marceau <fabien.marceau@gmail.com>
2023-01-29 22:38:39 +0100

    ok range mais pb webkit

--- index.js
+++ index.js
@@ -2,45 +2,37 @@ Vue.component("product_ordered", {
   template: `
         <div class = "container_ordered">
             <p class="product_order_txt">Votre choix: {{ nom }}</p>
-        <div class="sucre">
-            <button v-on:click ="sucrer()" class="sugar">Sucré</button>
+        <div class="container_sucre">
+            <button v-on:click ="sucrer()" class="sugar_button">Sucré</button>
+            <button v-on:click ="sucrer()" class="sugar_button">Non sucré</button>
              </div>
         </div>`,
   props: ["nom"],
-    methods: {
-        sucrer: function () {
-            this.$emit("go");
-        }
-    }  
+  methods: {
+    sucrer: function () {
+      this.$emit("go");
+    },
+  },
 });
-
-// Vue.component("sugar_selection", {
-//   template: `
-//         <div class = "container_sugarSelection">
-//     <input type="range" v-on:click="range()"></input> 
-//         </div>`,
-//   methods: {
-//    range: function () {
-//       this.$emit("bam");
-//     },
-//   },
-// });
 
 var app = new Vue({
   el: "#app",
   data: {
     array: ["Espresso", "Double Espresso", "Cappucino", "Américain", "Allongé", "Ristretto", "Thé", "Infusion", "Chocolat chaud"],
     panier: [],
+    isDisplay: false,
+    value: 0,
+    percentage: "%",
   },
 
-    methods: {
-        order: function (produits) {
-            console.log(produits);
-            this.panier.push(produits);
-        },
-        sugar_option: function () {
-            console.log("ok");
-            
-        },
-    }
+  methods: {
+    order: function (produits) {
+      console.log(produits);
+      this.panier.push(produits);
+    },
+    sugar_option: function () {
+      console.log("ok");
+      this.isDisplay = true;
+    },
+  },
 });
